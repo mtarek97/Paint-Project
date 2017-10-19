36d3afc4353716c4a715c8a7505e0bfc710ee740
ma7moud_tarek97 <ma7moud.tarek97@gmail.com>
2017-10-19 11:51:39 +0200

    initialize again

---
+++ src/paint/eg/edu/alexu/csd/oop/draw/cs62_67/main.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paint.eg.edu.alexu.csd.oop.draw.cs62_67;
+
+/**
+ *
+ * @author hp
+ */
+public class main {
+    
+}
